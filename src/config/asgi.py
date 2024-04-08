331014b86ae04f9c8d567e0c18a039c1ce50650f
Elliot Mitchum <elliotmitchum@gmail.com>
2024-04-09 07:02:49 +1000

    feat: use boilerplate

--- src/config/asgi.py
+++ src/config/asgi.py
@@ -1,7 +1,7 @@
 """
 ASGI config for mimosa project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+It exposes the ASGI callable as a module-level variable named `application`.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mimosa.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_asgi_application()
--- src/config/urls.py
+++ src/config/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
+from django.urls import include
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("up/", include("up.urls")),
+    path("", include("pages.urls")),
+    path("admin/", admin.site.urls),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
