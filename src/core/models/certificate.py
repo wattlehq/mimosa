668722be5d7e9aab3d7e9e9ecfc1bc950f25c860
Elliot Mitchum <elliot.mitchum@ellioseven.com.au>
2024-06-16 14:35:22 +1000

    feat: add cert model timestamps

--- src/core/models/certificate.py
+++ src/core/models/certificate.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Certificate(models.Model):
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
     name = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.choice_text
+        return self.name
