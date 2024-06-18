3eaee82b694e807389ff9576b2bd8e8785c1ed9b
Elliot Mitchum <elliot.mitchum@ellioseven.com.au>
2024-06-19 07:57:13 +1000

    fix: fee name to 254

--- src/core/models/fee.py
+++ src/core/models/fee.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Fee(models.Model):
-    name = models.CharField(max_length=120)
+    name = models.CharField(max_length=254)
 
     def __str__(self):
         return self.name
