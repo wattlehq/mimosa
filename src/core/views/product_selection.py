80a48310596a05eaaa5ca3a19f26a09501a5201a
Daniel Navarro <daniel.navarro.au@gmail.com>
2024-07-26 17:00:45 +1000

    feat: get certificate and fee

--- src/core/views/product_selection.py
+++ src/core/views/product_selection.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+from core.models.certificate import Certificate
+from core.models.fee import Fee
 
 def product_selection(request):
-    return render(request, 'pages/product_selection.html')
+    certificates = Certificate.objects.all()
+    fees = Fee.objects.all()
+    context = {
+        'certificates': certificates,
+        'fees': fees
+    }
+    return render(request, 'pages/product_selection.html', context)
+
