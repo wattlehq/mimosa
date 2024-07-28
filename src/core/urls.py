b5b147d7e915dedcaf194fb76c4ee4c4e1ecd248
Elliot Mitchum <elliotmitchum@gmail.com>
2024-07-29 07:10:55 +1000

    feat: implement success page

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
+from core.views.api import api_property_search
+from core.views.find_parcel import FindParcel
 from core.views.home import home
 from core.views.order import order
-from core.views.find_parcel import FindParcel
-from core.views.api import api_property_search
+from core.views.success import success
 from core.webhooks.stripe import webhook_stripe
 
 urlpatterns = [
     path("", home, name="home"),
+    path("success/", success, name="success"),
     path("webhook/stripe", webhook_stripe, name="webhook-stripe"),
     path("certificate-order/", FindParcel.as_view(), name="find_parcel"),
     path("api/property/search/",
---
+++ src/core/views/success.py
@@ -0,0 +1,15 @@
+import os
+
+from django import get_version
+from django.conf import settings
+from django.shortcuts import render
+
+
+def success(request):
+    context = {
+        "debug": settings.DEBUG,
+        "django_ver": get_version(),
+        "python_ver": os.environ["PYTHON_VERSION"]
+    }
+
+    return render(request, "pages/success.html", context)
