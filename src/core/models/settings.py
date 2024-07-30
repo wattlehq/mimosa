0f203f1c03349e668287cd36a93fddf583b9592c
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2024-07-30 17:35:02 +1000

    feat: add council_email field to Settings model
    - Created migration 0026_settings_council_email.py to add council_email field
    - Applied migrations to update the database schema
    - This new field will be used for sending order notifications

--- src/core/models/settings.py
+++ src/core/models/settings.py
@@ -1,8 +1,16 @@
 from django.db import models
+from django.core.validators import EmailValidator
 
 
 class Settings(models.Model):
     logo = models.ImageField(upload_to="logos/", blank=True, null=True)
+    council_email = models.EmailField(
+        max_length=254,
+        blank=True,
+        null=True,
+        validators=[EmailValidator()],
+        help_text="Email for sending customers order notifications"
+    )
 
     def __str__(self):
         return "Settings"
