d492490cd6f24f3138f31e35e71cc5f02a77df0f
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2024-07-31 12:11:06 +1000

    fix: add relevant defaults to prevent TypeErrors

--- src/app/settings.py
+++ src/app/settings.py
@@ -190,7 +190,7 @@ if DEBUG:
 else:
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_HOST = os.environ.get("EMAIL_HOST")
-    EMAIL_PORT = int(os.environ.get("EMAIL_PORT"))
-    EMAIL_USE_TLS = bool(strtobool(os.environ.get("EMAIL_USE_TLS")))
+    EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
+    EMAIL_USE_TLS = bool(strtobool(os.environ.get("EMAIL_USE_TLS", "true")))
     EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
     EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
