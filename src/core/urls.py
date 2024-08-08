1318a1cc1cd4119a65c219f1c245fdf2c750d041
Elliot Mitchum <elliotmitchum@gmail.com>
2024-08-09 05:03:16 +1000

    feat: rename route

--- src/core/urls.py
+++ src/core/urls.py
@@ -3,15 +3,12 @@ from django.urls import path
 from core.views.api import api_property_search
 from core.views.cancel import cancel
 from core.views.find_parcel import FindParcel
-from core.views.home import home
 from core.views.order import order
 from core.views.success import success
 from core.webhooks.stripe import webhook_stripe
 
 urlpatterns = [
-    path("", home, name="home"),
-    # @todo Rename
-    path("certificate-order/", FindParcel.as_view(), name="find_parcel"),
+    path("", FindParcel.as_view(), name="find_parcel"),
     path("order/<uuid:order_hash>/", order, name="order"),
     # Payment:
     path("success/", success, name="success"),
