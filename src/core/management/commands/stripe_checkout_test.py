a85e1a92633b55a26a6202216d9d7819e6d148e5
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2024-08-09 10:48:27 +1000

    feat: update stripe_checkout_test to use create_order_session and --random flag

--- src/core/management/commands/stripe_checkout_test.py
+++ src/core/management/commands/stripe_checkout_test.py
@@ -1,90 +1,17 @@
 from django.core.management.base import BaseCommand
-from django.urls import reverse
 
-from core.services.utils.random_order import generate_random_request
-from core.services.utils.stripe_service import StripeService
+from core.services.test.generate_random_order import generate_random_order
+from core.services.order.create_order_session import create_order_session
 
-# Input the "lines" value if you aren't using the --random flag
 manual_request = {
     "property_id": 1,
-    "lines": [{"certificate_id": 1, "fee_id": 1}, {"certificate_id": 2}],
+    "order_lines": [{"certificate_id": 13, "fee_id": 1}, {"certificate_id": 12}],
+    "customer_name": "Vitalik Buterin",
+    "customer_company_name": "ETH Inc.",
 }
 
 
-def create_line_items():
-    line_items = []
-
-    for value in request["lines"]:
-        certificate_id = value["certificate_id"]
-        certificate = Certificate.objects.get(pk=certificate_id)
-
-        line_items.append(
-            {"price": certificate.stripe_price_id, "quantity": 1}
-        )
-
-        if "fee_id" in value:
-            fee_id = value["fee_id"]
-            is_valid_fee = certificate.fees.filter(id=fee_id).exists()
-            if is_valid_fee:
-                fee = Fee.objects.get(pk=fee_id)
-                line_items.append(
-                    {"price": fee.stripe_price_id, "quantity": 1}
-                )
-
-    return line_items
-
-
-def create_stripe_checkout_session(order_session: OrderSession):
-    line_items = create_line_items()
-    return stripe.checkout.Session.create(
-        line_items=line_items,
-        metadata={"order_session_pk": order_session.id},
-        mode="payment",
-        success_url=get_site_url() + reverse("order_success"),
-        cancel_url=get_site_url() + reverse("order_form"),
-    )
-
-
-def save_order_session():
-    property_obj = Property.objects.get(id=request["property_id"])
-    order_session = OrderSession(property=property_obj)
-    order_session.save()
-
-    for value in request["lines"]:
-        certificate_id = value["certificate_id"]
-        certificate = Certificate.objects.get(id=certificate_id)
-        order_line = OrderSessionLine.objects.create(
-            order_session=order_session,
-            certificate=certificate,
-            cost_certificate=certificate.price,
-        )
-
-        if "fee_id" in value:
-            fee_id = value["fee_id"]
-            fee = Fee.objects.get(id=fee_id)
-            is_valid_fee = certificate.fees.filter(id=fee_id).exists()
-            if is_valid_fee:
-                order_line.fee = fee
-                order_line.cost_fee = fee.price
-                order_line.save()
-
-    return order_session
-
-
-def update_order_session(order_session: OrderSession, stripe_checkout_id: str):
-    order_session.stripe_checkout_id = stripe_checkout_id
-    order_session.save()
-
-
 class Command(BaseCommand):
-    """
-    Django management command to create a Stripe checkout instance for testing.
-
-    This command allows for the creation of a Stripe checkout session with
-    either predefined or randomly generated order details.
-
-    """
-
     help = "Create a Stripe checkout instance"
 
     def add_arguments(self, parser):
@@ -95,42 +22,25 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        """
-        Execute the command to create a Stripe checkout instance.
-
-        This method initialises the Stripe service, generates or uses a
-        predefined order request, creates an OrderSession, creates a Stripe
-        checkout session, and updates the OrderSession with the Stripe
-        checkout ID.
-
-        Args:
-            *args: Variable length argument list.
-            **options: Arbitrary keyword arguments. Includes 'random' flag.
-
-        Raises:
-            Exception: For any errors that occur during the process.
-        """
-        StripeService.initialise()  # Initialise Stripe API key
-
         if options["random"]:
-            request = generate_random_request()
+            request = generate_random_order()
         else:
             request = manual_request
 
         self.stdout.write(self.style.SUCCESS(f"Generated request: {request}"))
 
         try:
-            order_session = StripeService.save_order_session(request)
-            stripe_checkout = StripeService.create_stripe_checkout_session(
-                order_session, request
-            )
-            StripeService.update_order_session(
-                order_session, stripe_checkout.stripe_id
+            result = create_order_session(
+                property_id=request["property_id"],
+                order_lines=request["order_lines"],
+                customer_name=request["customer_name"],
+                customer_company_name=request["customer_company_name"],
             )
 
-            self.stdout.write(
-                self.style.SUCCESS(f"Checkout URL: {stripe_checkout.url}")
-            )
+            if result["success"]:
+                self.stdout.write(self.style.SUCCESS(f"Checkout URL: {result['checkout_url']}"))
+            else:
+                self.stdout.write(self.style.ERROR(f"Error: {result['error']}"))
 
         except Exception as e:
             self.stdout.write(self.style.ERROR(f"An error occurred: {str(e)}"))
