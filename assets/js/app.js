790358a94bbfa46baaffe3f6a00d6ec43f6dad23
Elliot Mitchum <elliotmitchum@gmail.com>
2024-08-20 07:23:34 +1000

    feat: fix lints

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
-import { OrderFindParcel } from "./core/orderFindParcel.js";
-import { OrderForm } from "./core/orderForm.js";
+import { OrderFindParcel } from './core/orderFindParcel.js'
+import { OrderForm } from './core/orderForm.js'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const inputSelectionTarget = '.order-form input[name="property_id"]';
-  new OrderFindParcel(inputSelectionTarget); // eslint-disable-line no-new
-  new OrderForm(); // eslint-disable-line no-new
-});
+  const inputSelectionTarget = '.order-form input[name="property_id"]'
+  new OrderFindParcel(inputSelectionTarget) // eslint-disable-line no-new
+  new OrderForm() // eslint-disable-line no-new
+})
