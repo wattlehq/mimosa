92924035c8c0774ddbddcaf8ed7de009fbb09860
Daniel Navarro <daniel.navarro.au@gmail.com>
2024-08-27 19:05:18 +1000

    fix formatting

--- src/core/services/certificate/group_items.py
+++ src/core/services/certificate/group_items.py
@@ -17,19 +17,21 @@ def group_items_by_parent(certificates):
             grouped_dict[certificate] = certificate.child_certificates.all()
     return grouped_dict
 
+
 def build_child_parent_map(certificates):
     """
-    Builds a dictionary that maps each certificate to a list of its parent certificates.
-    
+    Builds a dictionary that maps each certificate to a list of its
+    parent certificates.
+
     Args:
         certificates (list): A list of certificates.
-    
+
     Returns:
-        dict: A dictionary where the keys are certificates, and the values are lists
-        of the parent certificates for each key.
+        dict: A dictionary where the keys are certificates, and the values
+        are lists of the parent certificates for each key.
     """
     child_parent_map = {cert: [] for cert in certificates}
     for certificate in certificates:
         for child in certificate.child_certificates.all():
             child_parent_map[child].append(certificate)
-    return child_parent_map 
+    return child_parent_map
