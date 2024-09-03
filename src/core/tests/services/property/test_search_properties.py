399191e6c63b2cc827fde3618ee84f8b92b4d236
Elliot Mitchum <elliotmitchum@gmail.com>
2024-09-04 07:28:14 +1000

    Add street address search test

--- src/core/tests/services/property/test_search_properties.py
+++ src/core/tests/services/property/test_search_properties.py
@@ -6,7 +6,7 @@ from core.services.property.search_properties import search_properties
 
 class SearchPropertiesTest(TestCase):
 
-    def test_search_properties(self):
+    def test_search_properties_by_details(self):
         prop = Property(
             assessment="10",
             lot="100",
@@ -22,6 +22,26 @@ class SearchPropertiesTest(TestCase):
 
         search = search_properties(
             lot="10", section="1000", deposited_plan="1337"
+        )
+
+        self.assertEqual(search.first().pk, prop.pk)
+
+    def test_search_properties_by_street(self):
+        prop = Property(
+            assessment="10",
+            lot="100",
+            section="1000",
+            deposited_plan="1337",
+            address_street="123",
+            address_suburb="Fake St.",
+            address_state="Fake Town",
+            address_post_code="2000",
+        )
+
+        prop.save()
+
+        search = search_properties(
+            street_address="123 Fake St."
         )
 
         self.assertEqual(search.first().pk, prop.pk)
